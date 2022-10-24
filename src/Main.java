6758aa5ebde554fa726f05aab5d0a24c02e30958
Alatartsev1 <chillkiller337@gmail.com>
2022-10-24 14:27:02 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("24 вересня 14 годин 5 хвилин, лабораторна робота №2, Алатарцев М.О.");
+        System.out.println("Зміна в веб-браузері");
+    }
+}

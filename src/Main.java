c58669687c5d6bef3b2dcea7f06874b0e31fccec
Alatartsev <95185436+Alatartsev1@users.noreply.github.com>
2022-10-24 14:30:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("24 вересня 14 годин 5 хвилин, лабораторна робота №2, Алатарцев М.О.");
-        System.out.println("Зміна в веб-браузері");
+        System.out.println("Зміна в веб-браузері номер 1");
     }
 }
